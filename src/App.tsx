6446297a99b1b4e51bf17fd1f2343230f754dc37
lucascorreaa <lucas.lca14@gmail.com>
2025-01-17 14:57:38 -0300

    feat: added messages array

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+
+  // Array com todas as mensagens que serão exibidas
+  const messages = [
+    'Ei dev, tranks?!',
+    'Professor Corrêa aqui, alguns já me conhecem, outros não, mas tudo bem, vamos lá!',
+    'Estou aqui hoje para demonstrar uma gameficação de conversa em React, como no jogo de pokemon fire red...',
+    'Vou usar React, Typescript, um pouco de CSS e HTML para fazer isso...',
+    'Dentro do React, vou usar alguns Hooks, como:',
+    'UseState para gerenciar o indice da mensagem atual, no qual está lendo agora...',
+    'UseEffect para adicionar um listener para capturar o evento de pressionar a tecla enter, como você está fazendo agora...',
+    'Uma função para avançar para a próxima mensagem...',
+    'Um Array com todas as mensagens que serão exibidas...',
+    'E por fim, um componente de mensagem que recebe a mensagem atual e a exibe na tela...',
+    'Vou usar Github para versionar o código, a Vercel para hospedar o site...',
+    'Aqui está o link do repositório do Github: https://github.com/lucascorreaa/react-game-conversation',
+    'Quer saber mais sobre mim? Acesse meu site: https://github.com/lucascorreaa',
+    'Para contratar meus serviços, acesse: suportededomingo.com.br',
+    'Como todo o jogo de RPR da Game Freak, não tem como voltar as mensagens, seja diferentes dos iguais, então, até a próxima dev!',
+  ]
+
+  return (
+    <div className="app-container">
+      <h1>Ei Dev, Tranks?!</h1>
+    </div>
+  )
+}
+
+export default App
